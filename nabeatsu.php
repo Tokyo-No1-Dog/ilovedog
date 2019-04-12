55b3f8cf399c10561060600a88a88a86efbfd541
Tokyo-No1-Dog <49548447+Tokyo-No1-Dog@users.noreply.github.com>
2019-04-12 16:17:28 +0900

    Create nabeatsu.php
    世界のナベアツさんの「3のつく数字のときだけアホになる」プログラムです。

---
+++ nabeatsu.php
@@ -0,0 +1,11 @@
+<?php
+    $count = 0;
+    for($i=1896;$i<=2020;$i=$i+4) {
+        $count = $count + 1;
+        if($i==1916 || $i==1940 || $i==1944) {
+            continue;
+        }
+        echo "第".$count."回";
+        echo $i."年";
+        echo "\n";
+    }
